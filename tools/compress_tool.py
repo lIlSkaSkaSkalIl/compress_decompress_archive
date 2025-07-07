8546c12992518f538f97212d525bf5b1b2f26bd7
lIlSkaSkaSkalIl <168249736+lIlSkaSkaSkalIl@users.noreply.github.com>
2025-07-07 14:30:52 +0700

    Update compress_tool.py

--- tools/compress_tool.py
+++ tools/compress_tool.py
@@ -1,4 +1,7 @@
-import os import zipfile import subprocess import time
+import os
+import zipfile
+import subprocess
+import time
 
 def format_size(size_bytes): for unit in ['B', 'KB', 'MB', 'GB', 'TB']: if size_bytes < 1024: return f"{size_bytes:.2f} {unit}" size_bytes /= 1024 return f"{size_bytes:.2f} PB"
 
