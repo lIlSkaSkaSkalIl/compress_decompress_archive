65e36d69f547126c5b5640f7babdd01bd09546b8
lIlSkaSkaSkalIl <168249736+lIlSkaSkaSkalIl@users.noreply.github.com>
2025-07-08 06:17:42 +0700

    Add 7zip and tar compress_tool.py

--- tools/compress_tool.py
+++ tools/compress_tool.py
@@ -2,8 +2,10 @@ from tools.zip import zip_folder
 from tools.unzip import unzip_file
 from tools.rar import rar_folder
 from tools.unrar import unrar_file
+from tools.sevenzip import seven_zip_folder
+from tools.tar import tar_folder
 
-def run_tool(metode, input_path, output_path):
+def run_tool(metode, input_path, output_path, tar_method="gz"):
     metode = metode.lower()
     print(f"\n📦 Metode yang dipilih: {metode.upper()}")
     
@@ -15,5 +17,9 @@ def run_tool(metode, input_path, output_path):
         rar_folder(input_path, output_path)
     elif metode == "unrar":
         unrar_file(input_path, output_path)
+    elif metode == "7z":
+        seven_zip_folder(input_path, output_path)
+    elif metode == "tar":
+        tar_folder(input_path, output_path, method=tar_method)
     else:
         raise ValueError("Metode tidak dikenal!")
